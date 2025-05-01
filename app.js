17f6bd3dbe9bd55b65811ef3d538108e3626bc26
Gustavo Silva Santiago <gustavosantiago.dev@outlook.com>
2025-05-01 13:56:11 -0300

    segunda parte

---
+++ app.js
@@ -0,0 +1,17 @@
+onload = () =>{
+    document.querySelector('#bt-0').onclick = () => digito(0)
+    document.querySelector('#bt-1').onclick = () => digito(1)
+    document.querySelector('#bt-2').onclick = () => digito(2)
+    document.querySelector('#bt-3').onclick = () => digito(3)
+    document.querySelector('#bt-4').onclick = () => digito(4)
+    document.querySelector('#bt-5').onclick = () => digito(5)
+    document.querySelector('#bt-6').onclick = () => digito(6)
+    document.querySelector('#bt-7').onclick = () => digito(7)
+    document.querySelector('#bt-8').onclick = () => digito(8)
+    document.querySelector('#bt-9').onclick = () => digito(9)
+    document.querySelector('#bt-mais').onclick = () => digito()
+    document.querySelector('#bt-menos').onclick = () => digito()
+    document.querySelector('#bt-multi').onclick = () => digito()
+    document.querySelector('#bt-div').onclick = () => digito()
+    document.querySelector('#bt-ac').onclick = limpa
+}
